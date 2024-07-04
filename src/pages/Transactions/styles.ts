ab818f31a1f1c5a87c12e3ba8c846a925dc4c778
Demis Russo <demisjunior@hotmail.com>
2024-07-04 20:56:01 -0300

    add transactions table

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,3 +1,42 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-styled
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionsContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+  /* margin-top: 1.5rem; */
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${({ theme }) => theme['gray-700']};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${({ theme, variant }) => variant === 'income'? theme['green-300'] : theme['red-300']};
+`
